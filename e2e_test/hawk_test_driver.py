2561d65d0824eb20493e3cebf371a01071f4e0f1
Dawei Pang <dawei.pang@suse.com>
2024-10-19 17:17:47 +0800

    202410191718

--- e2e_test/hawk_test_driver.py
+++ e2e_test/hawk_test_driver.py
@@ -334,9 +334,7 @@ class HawkTestDriver:
         elem.click()
         time.sleep(10*BIG_TIMEOUT)
         print(dir(elem))
-        time.sleep(7200)
-        print(f"{cluster} is active? " + elem.get_dom_attribute("class"))
-        time.sleep(7200)
+        print(f"{cluster} is active? " + elem.get_attribute("class"))
         elem = self.find_element(By.CLASS_NAME, 'close')
         print("Element is visible? " + str(elem.is_displayed()))
         if not elem:
