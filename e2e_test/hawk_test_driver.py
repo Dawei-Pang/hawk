1cd5c493c288f6c9804601890330002bef33987b
Aleksei Burlakov <aburlakov@suse.com>
2026-06-03 11:19:15 +0200

    Test: use another name for hot_primitive
    Use dum_primitive instead. So that it comes alphabetically
    before both stonith-sbd and fencing-sbd.

--- e2e_test/hawk_test_driver.py
+++ e2e_test/hawk_test_driver.py
@@ -26,8 +26,8 @@ class Error:
     PRIMITIVE_TARGET_ROLE_ERR = "Couldn't find value [Started] for primitive target-role"
     FENCING_ERR = "Couldn't find stonith-sbd menu to place it in maintenance mode"
     COOL_PRIMITIVE_ERR = "Couldn't find cool_primitive menu to edit"
-    HOT_PRIMITIVE_ERR = "Couldn't find hot_primitive menu to edit"
-    DUMMY_PRIMITIVE_ERR = "Couldn't find hot_primitive menu to edit"
+    DUM_PRIMITIVE_ERR = "Couldn't find dum_primitive menu to edit"
+    DUMMY_PRIMITIVE_ERR = "Couldn't find dummy_primitive menu to edit"
     FENCING_ERR_OFF = "Could not find Disable Maintenance Mode button for stonith-sbd"
     CRM_CONFIG_ADVANCED_ATTRIBUTES = "crm_config dropdown box shows the advanced attributes, but shouldn't"
 
@@ -79,7 +79,7 @@ class Xpath:
     FENCING_MAINT_OFF = '//a[contains(@href, "stonith-sbd") and contains(@title, "Disable Maintenance Mode")]'
     FENCING_MAINT_ON = '//a[contains(@href, "stonith-sbd/maintenance_on")]'
     COOL_PRIMITIVE_EDIT = '//a[contains(@href, "cool_primitive/edit")]'
-    HOT_PRIMITIVE_EDIT = '//a[contains(@href, "hot_primitive/edit")]'
+    DUM_PRIMITIVE_EDIT = '//a[contains(@href, "dum_primitive/edit")]'
     DUMMY_PRIMITIVE_EDIT = '//a[contains(@href, "dummy_primitive/edit")]'
     TARGET_ROLE_FORMAT = '//select[contains(@class, "form-control") and contains(@name, "{}[meta][target-role]")]'
     TARGET_ROLE_STARTED = '//option[contains(@value, "tarted")]'
@@ -647,20 +647,20 @@ class HawkTestDriver:
 
     def test_copy_primitive(self):
         '''
-        Copy the cool_primitive into hot_primitive. This assumes the cool_primitive
+        Copy the cool_primitive into dum_primitive. This assumes the cool_primitive
         resource is the first entry in the resources table
         Returns:
             boolean: True if successful or False if failed
         '''
         if self.find_element(By.XPATH, Xpath.RSC_ROWS):
-            print("TEST: test_copy_primitive: cool_primitive --> cool_primitive + hot_primitive")
+            print("TEST: test_copy_primitive: cool_primitive --> cool_primitive + dum_primitive")
             resource_number_from_top = 1 # 1. cool_primitive, 2. stonith-sbd
             # First, click on Edit
             time.sleep(2)
             self.check_and_click_by_xpath(Error.COOL_PRIMITIVE_ERR, [Xpath.DROP_DOWN_FORMAT.format(resource_number_from_top),
                                                                      Xpath.COOL_PRIMITIVE_EDIT])
 
-            # Second, copy the cool_primitive into hot_primitive
+            # Second, copy the cool_primitive into dum_primitive
             time.sleep(2) # to redirect
             # try to find '<a>Copy</a>' for 10 seconds (Ruby hawk),
             # if none --> look for '<button name="copy"> (Go hawk)
@@ -671,7 +671,7 @@ class HawkTestDriver:
             copy_btn.click()
 
             time.sleep(7) # to fetch meta attributes
-            self.fill_value("primitive[id]", "hot_primitive")
+            self.fill_value("primitive[id]", "dum_primitive")
 
             submit_btn = self.find_element(By.NAME, "submit")
             if not submit_btn:
@@ -680,33 +680,33 @@ class HawkTestDriver:
             submit_btn.click()
 
             if self.verify_success():
-                print("INFO: cool_primitive successfully copied into hot_primitive")
+                print("INFO: cool_primitive successfully copied into dum_primitive")
                 return True
-        print("ERROR: failed to copy cool_primitive into hot_primitive")
+        print("ERROR: failed to copy cool_primitive into dum_primitive")
         return False
 
     def test_rename_primitive(self):
         '''
-        Rename the hot_primitive to dummy_primitive. This assumes the hot_primitive
+        Rename the dum_primitive to dummy_primitive. This assumes the dum_primitive
         resource is the second entry in the resources table
         Returns:
             boolean: True if successful or False if failed
         '''
         if self.find_element(By.XPATH, Xpath.RSC_ROWS):
-            print("TEST: test_rename_primitive: hot_primitive --> dummy_primitive")
-            resource_number_from_top = 2 # 1. cool_primitive, 2. hot_primitive, 3. stonith-sbd
+            print("TEST: test_rename_primitive: dum_primitive --> dummy_primitive")
+            resource_number_from_top = 2 # 1. cool_primitive, 2. dum_primitive, 3. stonith-sbd
 
             # First, stop the cool_primitive
             time.sleep(2)
-            self.check_and_click_by_xpath(Error.HOT_PRIMITIVE_ERR,[Xpath.STOP_PRIMITIVE_FORMAT.format(resource_number_from_top),
+            self.check_and_click_by_xpath(Error.DUM_PRIMITIVE_ERR,[Xpath.STOP_PRIMITIVE_FORMAT.format(resource_number_from_top),
                                                                     Xpath.COMMIT_BTN_DANGER])
 
             # Second, click on Edit
             time.sleep(BIG_TIMEOUT)
-            self.check_and_click_by_xpath(Error.HOT_PRIMITIVE_ERR, [Xpath.DROP_DOWN_FORMAT.format(resource_number_from_top),
-                                                                     Xpath.HOT_PRIMITIVE_EDIT])
+            self.check_and_click_by_xpath(Error.DUM_PRIMITIVE_ERR, [Xpath.DROP_DOWN_FORMAT.format(resource_number_from_top),
+                                                                     Xpath.DUM_PRIMITIVE_EDIT])
 
-            # Third, rename the cool_primitive to hot_primitive
+            # Third, rename the cool_primitive to dum_primitive
             time.sleep(BIG_TIMEOUT) # wait the redirect finishes
             # try to find '<a>Rename</a>' for 10 seconds (Ruby hawk),
             # if none --> look for '<button name="rename"> (Go hawk)
@@ -733,9 +733,9 @@ class HawkTestDriver:
             submit_btn.click()
 
             if self.verify_success():
-                print("INFO: hot_primitive successfully renamed into dummy_primitive")
+                print("INFO: dum_primitive successfully renamed into dummy_primitive")
                 return True
-        print("ERROR: failed to rename hot_primitive into dummy_primitive")
+        print("ERROR: failed to rename dum_primitive into dummy_primitive")
         return False
 
     def test_delete_primitive(self):
